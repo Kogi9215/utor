42275da5be2977d50e31bb3f655736697c7de576
Kogi9215 <82638438+Kogi9215@users.noreply.github.com>
2021-10-22 01:10:09 +0800

    Update Event.py

--- Event.py
+++ Event.py
@@ -9,8 +9,8 @@ from disnake.ext import commands
 clss Event(Cog_extension):
   @commands.Cog.listener()
   async def on_message(self,msg):
-  if msg.author == '671366378351230996':
-    await msg.channel.send('test')
+    if msg.author == '671366378351230996':
+      await msg.channel.send('test')
     
 def setup(bot):
   bot.add_cog(Event(bot))
